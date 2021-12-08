230ee6cb2fc9c394ec57ecade2dcd1eb0dd0a0ee
Sarang S Nair <92372142+sarangs1621@users.noreply.github.com>
2021-12-08 08:41:34 +0530

    Add files via upload

---
+++ Arithmetic Operation/try.py
@@ -0,0 +1,8 @@
+import example
+print("Addition Operator : ",example.add(4,10))
+print("Subtraction Operator :",example.subs(15,7))
+print("Multiplication Operator : ",example.multi(10,10))
+print("Division Operator : ",example.div(99,3))
+print("Floor division :",example.floor(3,8))
+print("Modulus Operator :",example.modulas(10,2))
+print("Exponentiation Operator :",example.expo(2,8))
