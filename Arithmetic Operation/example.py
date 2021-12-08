93429d9c210b3b2c7407979eb529037d5463b570
Sarang S Nair <92372142+sarangs1621@users.noreply.github.com>
2021-12-08 08:41:10 +0530

    Create example.py

---
+++ Arithmetic Operation/example.py
@@ -0,0 +1,29 @@
+# Python Module example
+
+def add(a, b):
+   result = a + b
+   return result
+
+def subs(a, b):
+   result = a - b
+   return result
+
+def multi(a, b):
+   result = a * b
+   return result
+
+def div(a, b):
+   result = a / b
+   return result
+
+def floor(a, b):
+   result = a // b
+   return result
+
+def modulas(a, b):
+   result = a % b
+   return result
+
+def expo(a, b):
+   result = a ** b
+   return result
